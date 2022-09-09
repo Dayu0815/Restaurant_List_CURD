0c375be41bebcc2e1d528788688d43b39e422462
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-09 14:40:50 +0800

    feat: modify  models/Restaurant.js

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -7,7 +7,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
 
-  name: { type: String, required: true },
+  name: {
+    type: String,  //資料型別是字串
+    required: true //這是個必填欄位
+  },
+
   name_en: { type: String, required: true },
   category: { type: String, required: true },
   image: { type: String, required: true },
