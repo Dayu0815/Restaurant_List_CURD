a377d0319665db67a0d25ff754890e3f863e3e71
Dayu0815 <dayu0826@yahoo.com.tw>
2022-09-11 13:28:00 +0800

    feat: modify views/edit.hbs

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,20 +7,20 @@ router.get("/new", (req, res) => {
   res.render("new")
 })
 
+// 設定路由 post 新增餐廳，記得刪除路由的前綴詞 /restaurants
+router.post("/new", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error))
+})
+
 // 設定路由 get 瀏覽特定一間餐廳，記得刪除路由的前綴詞 /restaurants
 router.get("/:id", (req, res) => {
   const id = req.params.id
   Restaurant.findById(id)
     .lean()
     .then(restaurantsData => res.render("show", { restaurantsData }))
-    .catch(err => console.log(err))
-})
-
-// 設定路由 post 新增餐廳，記得刪除路由的前綴詞 /restaurants
-router.post("/", (req, res) => {
-  Restaurant.create(req.body)
-    .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
+    .catch(error => console.log(error))
 })
 
 // 設定路由 get 編輯餐廳頁面，記得刪除路由的前綴詞 /restaurants
@@ -29,7 +29,7 @@ router.get("/:restaurantId/edit", (req, res) => {
   Restaurant.findById(restaurantId)
     .lean()
     .then(restaurantsData => res.render("edit", { restaurantsData }))
-    .catch(err => console.log(err))
+    .catch(error => console.log(error))
 })
 
 //methodOverride(路由覆蓋機制) 設定路由post，改成put 讀取查詢資料庫，接住修改後資料，送往資料庫儲存_ Update
@@ -37,7 +37,7 @@ router.put("/:id", (req, res) => {
   const id = req.params.id
   Restaurant.findByIdAndUpdate(id, req.body)
     .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(err => console.log(err))
+    .catch(error => console.log(error))
 })
 
 //methodOverride(路由覆蓋機制) 設定路由post，改成delete 讀取查詢資料庫，刪除特定資料_ Delete
@@ -45,7 +45,7 @@ router.delete("/:id", (req, res) => {
   const id = req.params.id
   Restaurant.findByIdAndDelete(id)
     .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
+    .catch(error => console.log(error))
 })
 //匯出 router路由器
 module.exports = router
